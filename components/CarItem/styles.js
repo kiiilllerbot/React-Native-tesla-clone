cf854d2fd4052891a63723644675c1ae76d8d99d
kiiillerbot <dev.shimolkhan@gmail.com>
2022-07-07 17:03:30 +0800

    Added Order Buttons

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,8 +1,9 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
 const styles = StyleSheet.create({
   carContainer: {
     width: "100%",
-    height: "100%",
+    height: Dimensions.get("window").height,
   },
   titles: {
     marginTop: "30%",
@@ -13,14 +14,25 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontWeight: "500",
   },
+  subtitleCTA: {
+    textDecorationLine: "underline",
+  },
   subtitle: {
+    fontSize: 16,
     color: "#5c5e62",
   },
+
   image: {
     width: "100%",
     height: "100%",
     resizeMode: "cover",
     position: "absolute",
+  },
+
+  buttonsContainer: {
+    position: "absolute",
+    bottom: 50,
+    width: "100%",
   },
 });
 
